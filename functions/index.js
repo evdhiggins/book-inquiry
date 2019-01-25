7064f31b865cac1080e827347597f1512a58946d
Evan Higgins <evdhiggins@gmail.com>
2019-01-24 21:42:22 -0600

    Limit CORS when not running in dev

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,14 @@
-const cors = require('cors')({ origin: true });
+const cors = require('cors');
 const express = require('express');
 const functions = require('firebase-functions');
 const { search } = require('./src/search');
 
 const app = express();
-app.use(cors);
+
+const corsOptions = {
+  origin: functions.config().env === 'dev' ? true : /https?:\/\/book-inquiry\.firebaseapp\.com/,
+};
+app.use(cors(corsOptions));
 
 app.get('/search', search);
 
