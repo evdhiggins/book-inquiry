cd3fb26bad8017b3ac78f848bd2587c7243d6b8a
Evan Higgins <evdhiggins@gmail.com>
2019-02-05 11:42:19 -0600

    Simplify ping store module tests

--- hosting/src/store/modules/Ping.test.js
+++ hosting/src/store/modules/Ping.test.js
@@ -2,31 +2,26 @@ const { PingModule } = require('./Ping');
 const { storeFunctions, fetchMockFactory } = require('../__mocks__/index');
 
 describe('pingServer', () => {
-  test('Call `fetch` once', async () => {
-    expect.assertions(1);
-    const fetchMock = fetchMockFactory();
-    const pingModule = new PingModule(storeFunctions, fetchMock);
+  const fetchMock = fetchMockFactory();
+  const pingModule = new PingModule(storeFunctions, fetchMock);
+
+  beforeAll(async (done) => {
     await pingModule.pingServer();
+    done();
+  });
+
+  test('Call `fetch` once', () => {
     expect(fetchMock.mock.calls.length).toBe(1);
   });
 
-  test('Call `fetch` with a valid url', async () => {
-    expect.assertions(1);
-    const fetchMock = fetchMockFactory();
-    const pingModule = new PingModule(storeFunctions, fetchMock);
-    await pingModule.pingServer();
-    const url = fetchMock.mock.calls[0][0];
-
+  test('Call `fetch` with a valid url', () => {
     // RegExp taken from https://stackoverflow.com/a/3809435
     const urlRegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/i;
+    const url = fetchMock.mock.calls[0][0];
     expect(url).toMatch(urlRegExp);
   });
 
-  test("Call `fetch` for the 'ping' endpoint", async () => {
-    expect.assertions(1);
-    const fetchMock = fetchMockFactory();
-    const pingModule = new PingModule(storeFunctions, fetchMock);
-    await pingModule.pingServer();
+  test("Call `fetch` for the 'ping' endpoint", () => {
     const url = fetchMock.mock.calls[0][0];
     expect(url).toMatch(/\/ping$/);
   });
